0392c78b30a7e82f6a9ebd84fb5873e4b36416ad
Johan Sanchez <johan.sanchez@ghtcorp.com>
2025-04-09 19:49:43 -0500

    Add project files.

---
+++ ViewDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ViewDemo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
